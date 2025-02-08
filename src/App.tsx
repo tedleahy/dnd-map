39d42c19cfb96fe7cf0e703d9da4afed46811f65
Ted Leahy <tleahy@uplandsoftware.com>
2025-02-08 23:00:51 +0000

    Add menu button that opens/closes a side menu box

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,38 @@
+import Box from '@mui/material/Box';
+import { useState } from 'react';
+import { IconButton, styled, Typography } from '@mui/material';
+import Menu from '@mui/icons-material/Menu';
+
+const DRAWER_WIDTH = 500;
+
+const MapContainer = styled(Box)`
+  display: flex;
+  flex-direction: column;
+  align-items: flex-start;
+  width: 100%;
+  height: 100vh;
+`;
+
 export default function App() {
-  return <></>
+  const [isDrawerOpen, setIsDrawerOpen] = useState(false);
+  const toggleDrawer = () => setIsDrawerOpen(!isDrawerOpen);
+
+  return (
+    <Box sx={{ display: 'flex' }}>
+      <Box
+        sx={{
+          display: isDrawerOpen ? 'block' : 'none',
+          width: DRAWER_WIDTH,
+          backgroundColor: 'red',
+        }}
+      >
+        <Typography variant='h6'>I am in the drawer!</Typography>
+      </Box>
+      <MapContainer>
+        <IconButton aria-label='Open side menu' size='large' onClick={toggleDrawer}>
+          <Menu fontSize='large' />
+        </IconButton>
+      </MapContainer>
+    </Box>
+  );
 }
