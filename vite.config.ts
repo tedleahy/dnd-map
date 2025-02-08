5d11fccc7b00d39d22cf9bc6a5b49e49a1c70d37
Ted Leahy <tleahy@uplandsoftware.com>
2025-02-08 22:49:24 +0000

    Add Material UI with styled components

--- vite.config.ts
+++ vite.config.ts
@@ -4,4 +4,9 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  resolve: {
+    alias: {
+      '@mui/styled-engine': '@mui/styled-engine-sc'
+    },
+  },
 })
