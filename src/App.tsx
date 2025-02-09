8f0ceb48763e2c58e565ab021a78c8c3f43398f5
Ted Leahy <tleahy@uplandsoftware.com>
2025-02-09 00:57:09 +0000

    Use MUI <Slide> component for side menu instead of <Box>

--- src/App.tsx
+++ src/App.tsx
@@ -2,8 +2,9 @@ import Box from '@mui/material/Box';
 import { useState } from 'react';
 import { IconButton, styled, Typography } from '@mui/material';
 import Menu from '@mui/icons-material/Menu';
+import Slide from '@mui/material/Slide';
 
-const DRAWER_WIDTH = 500;
+const MENU_WIDTH = 500;
 
 const MapContainer = styled(Box)`
   display: flex;
@@ -13,23 +14,25 @@ const MapContainer = styled(Box)`
   height: 100vh;
 `;
 
+const SideMenu = styled(Box)`
+  width: ${MENU_WIDTH}px;
+  border-right: 1px solid rgb(60 59 59 / 30%);
+  box-shadow: rgba(0, 0, 0, 0.2) 0 0 40px 0;
+`;
+
 export default function App() {
-  const [isDrawerOpen, setIsDrawerOpen] = useState(false);
-  const toggleDrawer = () => setIsDrawerOpen(!isDrawerOpen);
+  const [isMenuOpen, setisMenuOpen] = useState(false);
+  const toggleMenu = () => setisMenuOpen(!isMenuOpen);
 
   return (
-    <Box sx={{ display: 'flex' }}>
-      <Box
-        sx={{
-          display: isDrawerOpen ? 'block' : 'none',
-          width: DRAWER_WIDTH,
-          backgroundColor: 'red',
-        }}
-      >
-        <Typography variant='h6'>I am in the drawer!</Typography>
-      </Box>
+    <Box sx={{ display: 'flex', overflow: 'hidden' }}>
+      <Slide direction='right' in={isMenuOpen} mountOnEnter unmountOnExit>
+        <SideMenu>
+          <Typography variant='h6'>I am in the side menu!</Typography>
+        </SideMenu>
+      </Slide>
       <MapContainer>
-        <IconButton aria-label='Open side menu' size='large' onClick={toggleDrawer}>
+        <IconButton aria-label='Open side menu' size='large' onClick={toggleMenu} open={isMenuOpen}>
           <Menu fontSize='large' />
         </IconButton>
       </MapContainer>
